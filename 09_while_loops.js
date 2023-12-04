86894d346f1faa667ea81948a97aa98db439c394
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-04 16:30:17 +0530

    While Loops

---
+++ 09_while_loops.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+//while loop in JS;
+// let n = prompt("Enter the value of n")
+// let i = 0
+// while(i < n) {
+//   console.log(i)
+//   i++
+// }
+
+//do while loop in JS;
+//it is a while loop which is executed atleast once
+let n = prompt("Enter the value of n")
+let i = 0
+do {
+  console.log(i)
+  i++
+}
+while (i < n) 
