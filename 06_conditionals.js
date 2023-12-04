29eda0df0886a36100323544c84f5175e624372d
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-04 16:27:06 +0530

    Conditionals

---
+++ 06_conditionals.js
@@ -0,0 +1,8 @@
+let a = prompt("hey what's your age?") //it will ask a question
+// a is a string
+a = Number.parseInt(a) // Converting the string to number
+if (a > 0) {
+  alert("this is a valid age")
+} else {
+  alert("this is not a valid age")
+}
