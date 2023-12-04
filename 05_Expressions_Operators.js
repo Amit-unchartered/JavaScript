ed393bc1ce87c536c036622152b6e37ff32dd09d
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-04 16:22:58 +0530

    Expressions and Operators

---
+++ 05_Expressions_Operators.js
@@ -0,0 +1,35 @@
+console.log("Expressions and Operators")
+//Arithmetic Operators
+let a = 10;
+let b = 4;
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a / b = ", a / b)
+console.log("a ** b = ", a ** b)
+console.log("++a = ", ++a)
+console.log("a++ = ", a++)
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+
+//Assignemnt Operator
+let assignment = 1;
+assignment += 5 // same as assignment = assignment + 5
+console.log("a is now = ", a)
+
+//Comparison operators
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is", comp1 == comp2)
+console.log("comp1 != comp2 is", comp1 != comp2)
+console.log("comp1 === comp2 is", comp1 === comp2)// checks for value and type 
+console.log("comp1 !== comp2 is", comp1 !== comp2)
+
+//Logical operators
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+
