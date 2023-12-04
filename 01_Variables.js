48109da6d66459874cc41f075d84268ce3dc0b8c
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-04 12:35:44 +0530

    first commit

---
+++ 01_Variables.js
@@ -0,0 +1,9 @@
+console.log("Variables in JS")
+let a = 67
+console.log(a)
+a = "amit"
+console.log(a)
+
+//let 8amit = 7 // this will throw an error
+//let var = 7 //reserved keywords in JS can't be used for declaring variables
+//JS is case sensitive i.e AMIT and amit are different identifiers
