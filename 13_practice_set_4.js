829eafc61c12cd21ea065c30166e50196b84c793
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-01-02 18:34:43 +0530

    practice set 4

---
+++ 13_practice_set_4.js
@@ -0,0 +1,31 @@
+//problem 1
+let str = "har\""
+console.log(str.length)
+
+//problem2
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const word = 'fox';
+
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? 'is' : 'is not'
+  } in the sentence`,
+)
+//sentence.includes(word) --> returns true if the word is found in the sentence.
+// Expected output: "The word "fox" is in the sentence"
+
+//problem3
+let str2 = "Amit"
+console.log(str2.toUpperCase())
+
+//problem4
+let str3 = "Please give me Rs 1000"
+let amount = Number.parseInt(15) //changing the typeof amount from string to int
+console.log(amount)
+console.log(typeof amount) //it will be number since we have converted it from string to int
+
+//problem5
+let friend = "Deepika"
+friend[3] = "R" //this will not throw an error.
+console.log(friend) //Deepika will only be printed, it will not change the string, since the STRING IS IMMUTABLE
